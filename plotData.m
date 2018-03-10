14f1f1eacc9ff7b4694382c026be3c2265dfb63a
marionminayo <marionpenninah@gmail.com>
2018-03-10 04:58:12 +0300

    Add files via upload

---
+++ plotData.m
@@ -0,0 +1,225 @@
+function plotData(x, y)
+%PLOTDATA Plots the data points x and y into a new figure 
+%   PLOTDATA(x,y) plots the data points and gives the figure axes labels of
+%   population and profit.
+
+figure; % open a new figure window
+
+% ====================== YOUR CODE HERE ======================
+plot(6.1101, 17.592, 'rx', 'MarkerSize', 10);
+hold on;
+plot(5.5277, 9.1302, 'rx', 'MarkerSize', 10);
+hold on;
+plot(8.5186, 13.662, 'rx', 'MarkerSize', 10);
+hold on;
+plot(7.0032, 11.854, 'rx', 'MarkerSize', 10);
+hold on;
+plot(5.8598, 6.8233, 'rx', 'MarkerSize', 10);
+hold on;
+plot(8.3829, 11.886, 'rx', 'MarkerSize', 10);
+hold on;
+plot(7.4764, 4.3483, 'rx', 'MarkerSize', 10);
+hold on;
+plot(8.5781, 12, 'rx', 'MarkerSize', 10);
+hold on;
+plot(6.4862, 6.5987, 'rx', 'MarkerSize', 10);
+hold on;
+plot(5.0546, 3.8166, 'rx', 'MarkerSize', 10);
+hold on;
+plot(5.7107, 3.2522, 'rx', 'MarkerSize', 10);
+hold on;
+plot(14.164, 15.505, 'rx', 'MarkerSize', 10);
+hold on;
+plot(5.734, 3.1551, 'rx', 'MarkerSize', 10);
+hold on;
+plot(8.4084, 7.2258, 'rx', 'MarkerSize', 10);
+hold on;
+plot(5.6407, 0.71618, 'rx', 'MarkerSize', 10);
+hold on;
+plot(5.3794, 3.5129, 'rx', 'MarkerSize', 10);
+hold on;
+plot(6.3654, 5.3048, 'rx', 'MarkerSize', 10);
+hold on;
+plot(5.1301, 0.56077, 'rx', 'MarkerSize', 10);
+hold on;
+plot(6.4296, 3.6518, 'rx', 'MarkerSize', 10);
+hold on;
+plot(7.0708,5.3893, 'rx', 'MarkerSize', 10);
+hold on;
+plot(6.1891,3.1386, 'rx', 'MarkerSize', 10);
+hold on;
+plot(20.27,21.767, 'rx', 'MarkerSize', 10);
+hold on;
+plot(5.4901,4.263, 'rx', 'MarkerSize', 10);
+hold on;
+plot(6.3261,5.1875, 'rx', 'MarkerSize', 10);
+hold on;
+plot(5.5649,3.0825, 'rx', 'MarkerSize', 10);
+hold on;
+plot(18.945,22.638, 'rx', 'MarkerSize', 10);
+hold on;
+plot(12.828,13.501, 'rx', 'MarkerSize', 10);
+hold on;
+plot(10.957,7.0467, 'rx', 'MarkerSize', 10);
+hold on;
+plot(13.176,14.692, 'rx', 'MarkerSize', 10);
+hold on;
+plot(22.203,24.147, 'rx', 'MarkerSize', 10);
+hold on;
+plot(5.2524,-1.22, 'rx', 'MarkerSize', 10);
+hold on;
+plot(6.5894,5.9966, 'rx', 'MarkerSize', 10);
+hold on;
+plot(9.2482,12.134, 'rx', 'MarkerSize', 10);
+hold on;
+plot(5.8918,1.8495, 'rx', 'MarkerSize', 10);
+hold on;
+plot(8.2111,6.5426, 'rx', 'MarkerSize', 10);
+hold on;
+plot(7.9334,4.5623, 'rx', 'MarkerSize', 10);
+hold on;
+plot(8.0959,4.1164, 'rx', 'MarkerSize', 10);
+hold on;
+plot(5.6063,3.3928, 'rx', 'MarkerSize', 10);
+hold on;
+plot(12.836,10.117, 'rx', 'MarkerSize', 10);
+hold on;
+plot(6.3534,5.4974, 'rx', 'MarkerSize', 10);
+hold on;
+plot(5.4069,0.55657, 'rx', 'MarkerSize', 10);
+hold on;
+plot(6.8825,3.9115, 'rx', 'MarkerSize', 10);
+hold on;
+plot(11.708,5.3854, 'rx', 'MarkerSize', 10);
+hold on;
+plot(5.7737,2.4406, 'rx', 'MarkerSize', 10);
+hold on;
+plot(7.8247,6.7318, 'rx', 'MarkerSize', 10);
+hold on;
+plot(7.0931,1.0463, 'rx', 'MarkerSize', 10);
+hold on;
+plot(5.0702,5.1337, 'rx', 'MarkerSize', 10);
+hold on;
+plot(5.8014,1.844, 'rx', 'MarkerSize', 10);
+hold on;
+plot(11.7,8.0043, 'rx', 'MarkerSize', 10);
+hold on;
+plot(5.5416,1.0179, 'rx', 'MarkerSize', 10);
+hold on;
+plot(7.5402,6.7504, 'rx', 'MarkerSize', 10);
+hold on;
+plot(5.3077,1.8396, 'rx', 'MarkerSize', 10);
+hold on;
+plot(7.4239,4.2885, 'rx', 'MarkerSize', 10);
+hold on;
+plot(7.6031,4.9981, 'rx', 'MarkerSize', 10);
+hold on;
+plot(6.3328,1.4233, 'rx', 'MarkerSize', 10);
+hold on;
+plot(6.3589,-1.4211, 'rx', 'MarkerSize', 10);
+hold on;
+plot(6.2742,2.4756, 'rx', 'MarkerSize', 10);
+hold on;
+plot(5.6397,4.6042, 'rx', 'MarkerSize', 10);
+hold on;
+plot(9.3102,3.9624, 'rx', 'MarkerSize', 10);
+hold on;
+plot(9.4536,5.4141, 'rx', 'MarkerSize', 10);
+hold on;
+plot(8.8254,5.1694, 'rx', 'MarkerSize', 10);
+hold on;
+plot(5.1793,-0.74279, 'rx', 'MarkerSize', 10);
+hold on;
+plot(21.279,17.929, 'rx', 'MarkerSize', 10);
+hold on;
+plot(14.908,12.054, 'rx', 'MarkerSize', 10);
+hold on;
+plot(18.959,17.054, 'rx', 'MarkerSize', 10);
+hold on;
+plot(7.2182,4.8852, 'rx', 'MarkerSize', 10);
+hold on;
+plot(8.2951,5.7442, 'rx', 'MarkerSize', 10);
+hold on;
+plot(10.236,7.7754, 'rx', 'MarkerSize', 10);
+hold on;
+plot(5.4994,1.0173, 'rx', 'MarkerSize', 10);
+hold on;
+plot(20.341,20.992, 'rx', 'MarkerSize', 10);
+hold on;
+plot(10.136,6.6799, 'rx', 'MarkerSize', 10);
+hold on;
+plot(7.3345,4.0259, 'rx', 'MarkerSize', 10);
+hold on;
+plot(6.0062,1.2784, 'rx', 'MarkerSize', 10);
+hold on;
+plot(7.2259,3.3411, 'rx', 'MarkerSize', 10);
+hold on;
+plot(5.0269,-2.6807, 'rx', 'MarkerSize', 10);
+hold on;
+plot(6.5479,0.29678, 'rx', 'MarkerSize', 10);
+hold on;
+plot(7.5386,3.8845, 'rx', 'MarkerSize', 10);
+hold on;
+plot(5.0365,5.7014, 'rx', 'MarkerSize', 10);
+hold on;
+plot(10.274,6.7526, 'rx', 'MarkerSize', 10);
+hold on;
+plot(5.1077,2.0576, 'rx', 'MarkerSize', 10);
+hold on;
+plot(5.7292,0.47953, 'rx', 'MarkerSize', 10);
+hold on;
+plot(5.1884,0.20421, 'rx', 'MarkerSize', 10);
+hold on;
+plot(6.3557,0.67861, 'rx', 'MarkerSize', 10);
+hold on;
+plot(9.7687,7.5435, 'rx', 'MarkerSize', 10);
+hold on;
+plot(6.5159,5.3436, 'rx', 'MarkerSize', 10);
+hold on;
+plot(8.5172,4.2415, 'rx', 'MarkerSize', 10);
+hold on;
+plot(9.1802,6.7981, 'rx', 'MarkerSize', 10);
+hold on;
+plot(6.002,0.92695, 'rx', 'MarkerSize', 10);
+hold on;
+plot(5.5204,0.152, 'rx', 'MarkerSize', 10);
+hold on;
+plot(5.0594,2.8214, 'rx', 'MarkerSize', 10);
+hold on;
+plot(5.7077,1.8451, 'rx', 'MarkerSize', 10);
+hold on;
+plot(7.6366,4.2959, 'rx', 'MarkerSize', 10);
+hold on;
+plot(5.8707,7.2029, 'rx', 'MarkerSize', 10);
+hold on;
+plot(5.3054,1.9869, 'rx', 'MarkerSize', 10);
+hold on;
+plot(8.2934,0.14454, 'rx', 'MarkerSize', 10);
+hold on;
+plot(13.394,9.0551, 'rx', 'MarkerSize', 10);
+hold on;
+plot(5.4369,0.61705, 'rx', 'MarkerSize', 10);
+hold on;
+
+ylabel('Profit in $10,000s');
+xlabel('Population of City in 10,000s');
+% Instructions: Plot the training data into a figure using the 
+%               "figure" and "plot" commands. Set the axes labels using
+%               the "xlabel" and "ylabel" commands. Assume the 
+%               population and revenue data have been passed in
+%               as the x and y arguments of this function.
+%
+% Hint: You can use the 'rx' option with plot to have the markers
+%       appear as red crosses. Furthermore, you can make the
+%       markers larger by using plot(..., 'rx', 'MarkerSize', 10);
+
+
+% Plot the data
+% Set the y−axis label
+% Set the x−axis label
+
+
+
+% ============================================================
+
+end
